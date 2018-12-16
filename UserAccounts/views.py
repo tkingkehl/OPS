879fae69b3552f07c90198b5b1920e71e1ff4424
tkingkehl <ACancerousTwzlr@gmail.com>
2018-12-16 17:00:30 -0600

    doxy

--- UserAccounts/views.py
+++ UserAccounts/views.py
@@ -21,7 +21,7 @@ class Register(generic.CreateView):
 #context = {}
 	#return render(request, 'login.html', context)
 def login(request):
-"""! Redirects to the login page.
+"""!@brief Redirects to the login page.
 """
     return redirect("/accounts/login")
 	
